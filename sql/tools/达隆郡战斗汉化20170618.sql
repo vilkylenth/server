cdefa0456f4b881c8965a50b093573a108f5637e
vilkylenth <51523146@qq.com>
2017-06-18 16:13:50 +0800

    达隆郡战斗生物模型修正

---
+++ sql/tools/达隆郡战斗汉化20170618.sql
@@ -0,0 +1,3 @@
+-- 修复生物装备模型
+UPDATE `nworld`.`creature_equip_template_raw` SET `entry`='10948', `equipmodel1`='15826', `equipmodel2`='18665', `equipmodel3`='0', `equipinfo1`='218171138', `equipinfo2`='234948100', `equipinfo3`='0', `equipslot1`='781', `equipslot2`='14', `equipslot3`='0' WHERE (`entry`='10948');
+UPDATE `nworld`.`creature_equip_template_raw` SET `entry`='10952', `equipmodel1`='7426', `equipmodel2`='18665', `equipmodel3`='0', `equipinfo1`='218169346', `equipinfo2`='234948100', `equipinfo3`='0', `equipslot1`='13', `equipslot2`='14', `equipslot3`='0' WHERE (`entry`='10952');
